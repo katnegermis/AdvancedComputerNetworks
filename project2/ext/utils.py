2942a3aa9f2789a1f5c5512ab5d5cce1454f11b2
Michael Bang <michael@katnegermis.dk>
2014-05-16 18:40:15 +0200

    Move general stuff in to a mixin.

---
+++ project2/ext/utils.py
@@ -0,0 +1,92 @@
+# Copyright 2012 James McCauley
+#
+# This file is part of POX.
+#
+# POX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# POX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with POX.    If not, see <http://www.gnu.org/licenses/>.
+
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+log = core.getLogger()
+
+class Global:
+    controller = None
+
+
+class ControllerMixin(object):
+    def __init__(self, connection):
+        # Keep track of the connection to the switch so that we can
+        # send it messages!
+        self.connection = connection
+
+        # This binds our PacketIn event listener
+        connection.addListeners(self)
+
+        # Use this table to keep track of which ethernet address is on
+        # which switch port (keys are MACs, values are ports).
+        self.mac_to_port = {}
+
+    def send_packet(self, buffer_id, raw_data, out_port, in_port):
+        """
+        Sends a packet out of the specified switch port.
+        If buffer_id is a valid buffer on the switch, use that.    Otherwise,
+        send the raw data in raw_data.
+        The "in_port" is the port number that packet arrived on.    Use
+        OFPP_NONE if you're generating this packet.
+        """
+        msg = of.ofp_packet_out()
+        msg.in_port = in_port
+        if buffer_id != -1 and buffer_id is not None:
+            # We got a buffer ID from the switch; use that
+            msg.buffer_id = buffer_id
+        else:
+            # No buffer ID from switch -- we got the raw data
+            if raw_data is None:
+                # No raw_data specified -- nothing to send!
+                return
+            msg.data = raw_data
+
+        # Add an action to send to the specified port
+        action = of.ofp_action_output(port=out_port)
+        msg.actions.append(action)
+
+        # Send message to switch
+        self.connection.send(msg)
+
+    def _handle_PacketIn(self, event):
+        """
+        Handles packet in messages from the switch.
+        """
+        packet = event.parsed  # This is the parsed packet data.
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
+        packet_in = event.ofp  # The actual ofp_packet_in message.
+        self.handle_packet(packet, packet_in)
+
+    def flood(self, packet, packet_in):
+        self.send_packet(packet_in.buffer_id, packet_in.data,
+                         of.OFPP_FLOOD, packet_in.in_port)
+
+
+def launch():
+    """
+    Starts the component
+    """
+    def start_switch(event):
+        log.debug("Controlling %s" % (event.connection,))
+        Global.controller(event.connection)
+
+    core.openflow.addListenerByName("ConnectionUp", start_switch)
