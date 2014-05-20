b1cd050fd7c604fe7c10882a139bc4fd0b95eacc
Michael Bang <michael@katnegermis.dk>
2014-05-20 19:16:56 +0200

    Remove comments.

--- project2/topologies/topo-2h-3s-2h.py
+++ project2/topologies/topo-2h-3s-2h.py
@@ -1,13 +1,3 @@
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-
 from mininet.topo import Topo
 
 
